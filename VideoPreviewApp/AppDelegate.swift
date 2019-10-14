0bcdfc79894374549bee162105b388c898a6307c
Raj Maurya <rajmaurya8085@gmail.com>
2019-10-14 22:34:11 +0530

    Initial Commit

---
+++ VideoPreviewApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoPreviewApp
+//
+//  Created by Raj Maurya on 14/10/19.
+//  Copyright © 2019 Raj Maurya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoPreviewApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoPreviewApp
+//
+//  Created by Raj Maurya on 14/10/19.
+//  Copyright © 2019 Raj Maurya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
